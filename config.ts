99be5961323753efe7a47a0aa91b64874c7ce5bb
Benedict <benedict.arowo@gmail.com>
2024-07-01 11:32:12 +0300

    refactor: Removed IPInfo credentials

--- config.ts
+++ config.ts
@@ -5,7 +5,6 @@ dotenv.config({ path: "./.env" });
 const config: IConfig = {
 	PORT: parseInt(process.env.PORT as string) || 3000,
 	WEATHER_API_KEY: process.env.WEATHER_API_KEY as string,
-	IPINFO_TOKEN: process.env.IPINFO_TOKEN as string,
 };
 
 export default config;
@@ -13,5 +12,4 @@ export default config;
 interface IConfig {
 	PORT: number;
 	WEATHER_API_KEY: string;
-	IPINFO_TOKEN: string;
 }
